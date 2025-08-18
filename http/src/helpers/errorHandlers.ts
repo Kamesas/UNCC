311ca596909302a99a98a4f710569f9ccc454840
AlexSemak <semakaleksandr2014@gmail.com>
2025-08-18 09:11:27 +0300

    add the withErrorHandler HOF for book controllers

--- http/src/helpers/errorHandlers.ts
+++ http/src/helpers/errorHandlers.ts
@@ -1,24 +1,13 @@
-import { sendResponse } from "./httpHelpers.js";
-
-export async function errorHandler(
-  http: Http,
-  callback: (http: Http) => Promise<void>
-) {
-  try {
-    await callback(http);
-  } catch (error) {
-    console.error("error", error);
-    sendResponse(http.res, 500, { message: "Internal Server Error" });
-  }
-}
-
 export function withErrorHandler(callback: (http: Http) => Promise<void>) {
   return async (http: Http) => {
     try {
       await callback(http);
     } catch (error) {
-      console.error("error", error);
-      sendResponse(http.res, 500, { message: "Internal Server Error" });
+      console.error("\x1b[31m\x1b[1mwithErrorHandler error --->\x1b[0m", error);
+
+      http.res.statusCode = 500;
+      http.res.setHeader("Content-Type", "application/json");
+      http.res.end(JSON.stringify({ message: "Internal Server Error" }));
     }
   };
 }
--- http/src/index.ts
+++ http/src/index.ts
@@ -1,2 +1,1 @@
-import "./features/gemma-three/index.js";
 import "./server.js";
